ee98a939ab11714003821282e768105be27132e4
Jayne Borrer <jayneb@metropolia.fi>
2020-08-21 12:49:13 +0300

    Upto 2.18 done

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,36 +11,80 @@ const App = () => {
   const [showSearched, setShowSearched] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPeople => {
+        setPersons(initialPeople)
+      })
+      .catch(e => {
+        console.log('get error', e)
       })
   }, [])
 
+  const deletePerson = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(person.id)
+        .then(() => {
+          console.log('deleted', person)
+          setPersons(persons.filter(p => p.id !== person.id))
+        })
+        .catch(e => {
+          console.log('delete error', e)
+        })
+    }
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
+    const foundPerson = persons.find(p => p.name === newName)
     if (!checkNameExists(newName)) {
       const personObject = {
         name: newName,
         number: newNumber,
         id: persons.length + 1
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(e => {
+          console.log('create error', e)
+        })
     } else {
-      window.alert(`${newName} is already added to the phonebook`)
+      update(foundPerson, newNumber)
     }
   }
 
-  const peopleToShow = persons.filter(person => {
-    return person.name.toLowerCase().includes(showSearched.toLowerCase());
-  });
+  const update = (person, newNumber) => {
+    if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+      const editedPerson = { ...person, number: newNumber }
+      console.log('edited', editedPerson)
+      personService
+        .update(person.id, editedPerson)
+        .then(() => {
+          setPersons(persons.map(person => person.id === editedPerson.id ? editedPerson : person))
+          setNewNumber('')
+          setNewName('')
+        })
+        .catch(e => {
+          console.log('update error', e)
+        })
+    }
+  }
 
-  
+  const peopleToShow = () => {
+    if (showSearched === '') {
+      return persons
+    }
+    const temp = persons.filter(person => person.name.toLowerCase().includes(showSearched.toLowerCase()))
+    return temp
+  }
+
   const checkNameExists = (name) => {
     return persons.some(person => person.name === name)
   }
@@ -63,10 +107,10 @@ const App = () => {
       <Filter value={showSearched} onChange={handleNameSearched} />
       <h2>add a new</h2>
       <PersonForm onSubmit={addPerson} valueName={newName} onChangeName={handleNameChange} valueNumber={newNumber}
-      onChangeNumber={handleNumberChange} />     
+        onChangeNumber={handleNumberChange} />
       <h2>Numbers</h2>
       <div>
-        <Persons people={peopleToShow} />
+        <Persons people={peopleToShow()} deletePerson={deletePerson} />
       </div>
     </div>
   )
--- Part2/phonebook/src/Components/Persons.js
+++ Part2/phonebook/src/Components/Persons.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Persons = ({ people }) => {
+const Persons = ({ people, deletePerson }) => {
     return (
-        <div>{people.map(person => <div key={person.name}>{person.name} {person.number}</div>)}</div>
+        <div>{people.map(person => <div key={person.name}>{person.name} {person.number}<button onClick={() => deletePerson(person)}>delete</button></div>)}</div>
     )
 }
 
